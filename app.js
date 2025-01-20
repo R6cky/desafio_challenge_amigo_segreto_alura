b3296c6dcedf9ad060392d33591001623201deb2
r6cky <cleitonrocha321@hotmail.com>
2025-01-20 17:29:32 -0300

    create somethings for example, the array [ listaDeAmigos ], the variable [ ulHtml], and variable [ newtagLi ] and a logic for the [ adicionar amigo ] function

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+const listaDeAmigos = [];
+const inputName = document.querySelector(".input-name");
+const ulHtml = document.querySelector(".name-list");
+const newTagLi = document.createElement("li");
+
+function adicionarAmigo() {
+  const buttonHtml = document.querySelector(".button-add");
+  if (inputName.value.length == 0) {
+    alert("Por favor, insira um nome válido.");
+  }
+
+  listaDeAmigos.push(inputName.value);
+  ulHtml.innerHTML = "";
+  listaDeAmigos.forEach((elem) => {
+    const newTagLi = document.createElement("li");
+    newTagLi.innerHTML = elem;
+    ulHtml.append(newTagLi);
+  });
+}
+
+function sortearAmigo() {}
